a08f82439221f866629963544bba33ffbdabcb5f
Johan Van De Walle <jvandewallemuniz@gmail.com>
2020-05-22 20:40:20 +0200

    A new folder with current datetime added to keep history

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
-
+import datetime
 from dirsync import sync
 from pathlib import Path
 
 CONFIGURATION_FILE_PATH = 'conf.txt'
+DATETIME_FORMAT = '%Y%m%dT%H%M%S'
 
 
 def retrieve_from_file(file_path: Path):
@@ -16,16 +17,28 @@ def retrieve_from_file(file_path: Path):
     return lines[0], lines[1:]
 
 
-def process(file_path: Path):
+def store_backup(output_folder, folder_to_backup, now):
     '''
-    Retrives data from file, and syncs folders
+    Stores every folder into the ourput folder, concatenating current date time to keep history
     '''
-    output, to_backup = retrieve_from_file(file_path)
-
-    print('Storing into: "%s"' % output)
-    for folder in to_backup:
-        sync(folder, output / folder.stem, 'sync',
+    print('Storing into: "%s"' % output_folder)
+    for folder in folder_to_backup:
+        sync(folder, output_folder / now.strftime(DATETIME_FORMAT) / folder.stem, 'sync',
              purge=True, create=True, verbose=True)
+
+
+def process(file_path: Path, history_level: int):
+    '''
+    Retrives data from file, and syncs folders into a new folder with current datetime
+    '''
+    # Retrieving configuration from file
+    output_folder, folder_to_backup = retrieve_from_file(file_path)
+
+    # Getting current datetime to create a folder with this name to store this backup
+    now = datetime.datetime.now().replace(microsecond=0)
+
+    # Actually storing the backup
+    store_backup(output_folder, folder_to_backup, now)
 
 
 def main():
@@ -36,10 +49,12 @@ def main():
         description='Synchronizes all folders and files given in a txt file, into the destiny folder')
     parser.add_argument('--file_path', type=Path, default=Path(CONFIGURATION_FILE_PATH),
                         help='Path to the file that contains information. The first line of that file, is the destiny folder')
+    parser.add_argument('--history_level', type=int, default=10,
+                        help='Number of backups to safe before starting to delete the oldest one')
 
     args = parser.parse_args()
 
-    process(args.file_path)
+    process(args.file_path, args.history_level)
 
 
 if __name__ == '__main__':
