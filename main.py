601522cb53b84e1603d064e6f30a1edcd02ad0e5
Johan Van De Walle <jvandewallemuniz@gmail.com>
2020-05-22 21:12:26 +0200

    Keeps stored only as many backups as history_levels that is passed via command line argument

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import shutil
 import argparse
 import datetime
+
+from re import search
 from dirsync import sync
 from pathlib import Path
 
@@ -17,13 +21,29 @@ def retrieve_from_file(file_path: Path):
     return lines[0], lines[1:]
 
 
+def remove_old_backup(output_folder, history_level):
+    '''
+    Removes the oldes backup stored in the output folder if there are more that history_level
+    '''
+    # Lists all backups and sort then by creation date
+    folder_in_output_folder = sorted(
+        Path(output_folder).iterdir(), key=os.path.getmtime, reverse=True)
+    folder_in_output_folder = [f for f in folder_in_output_folder if search(
+        'backup_\d\d\d\d\d\d\d\dT\d\d\d\d\d\d', f.name) is not None]
+
+    while len(folder_in_output_folder) >= history_level:
+        # Removing the oldest folder and it's content. Let except so the error is catch by the launcher
+        folder_to_remove = folder_in_output_folder.pop()
+
+        shutil.rmtree(folder_to_remove)
+
+
 def store_backup(output_folder, folder_to_backup, now):
     '''
     Stores every folder into the ourput folder, concatenating current date time to keep history
     '''
-    print('Storing into: "%s"' % output_folder)
     for folder in folder_to_backup:
-        sync(folder, output_folder / now.strftime(DATETIME_FORMAT) / folder.stem, 'sync',
+        sync(folder, output_folder / 'backup_{}'.format(now.strftime(DATETIME_FORMAT)) / folder.stem, 'sync',
              purge=True, create=True, verbose=True)
 
 
@@ -36,6 +56,9 @@ def process(file_path: Path, history_level: int):
 
     # Getting current datetime to create a folder with this name to store this backup
     now = datetime.datetime.now().replace(microsecond=0)
+
+    # Removing the oldest backup in case there are more than history_level
+    remove_old_backup(output_folder, history_level)
 
     # Actually storing the backup
     store_backup(output_folder, folder_to_backup, now)
