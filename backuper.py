74c62c841297c27c49fb907f2630205b268b7397
Johan Van De Walle <jvandewallemuniz@gmail.com>
2020-05-25 14:43:10 +0200

    Added renaming of duplicates in case there's any

--- backuper.py
+++ backuper.py
@@ -4,7 +4,7 @@ import shutil
 import argparse
 import datetime
 
-from re import search
+from re import search, compile
 from dirsync import sync
 from pathlib import Path
 
@@ -45,7 +45,7 @@ def retrieve_configuration_from_file(file_path: Path):
     return output_folder, history_level, folders_to_backup
 
 
-def remove_old_backup(output_folder, history_level):
+def remove_old_backup(output_folder: Path, history_level: int):
     '''
     Removes the oldest backups stored in the output folder if there are more that history_level
     '''
@@ -62,12 +62,31 @@ def remove_old_backup(output_folder, history_level):
         shutil.rmtree(folder_to_remove)
 
 
-def store_backup(folder_to_backup):
+def store_backup(folder_to_backup: list):
     '''
     Stores every folder into the ourput folder, concatenating current date time to keep history
     '''
+    def get_duplicate_suffix(path: Path, name: str) -> str:
+        '''
+        Returns a suffix (N) where N is the number of duplicate, or empty if there's none
+        '''
+        suffix = ''
+        if path.exists():
+            regex = compile(name+'[\(\d+\)]*')
+            folders_in_path = [item.stem for item in path.iterdir()]
+
+            duplicates = list(filter(regex.match, folders_in_path))
+            if duplicates:
+                suffix = '({})'.format(len(duplicates))
+
+        return suffix
+
     for folder in folder_to_backup:
-        sync(folder['source'], folder['target'] / folder['source'].stem, 'sync',
+        # If there's a duplicate, add (N) to the target name
+        duplicate_sufix = get_duplicate_suffix(
+            folder['target'], folder['source'].stem)
+
+        sync(folder['source'], folder['target'] / (folder['source'].stem + duplicate_sufix), 'sync',
              purge=True, create=True, verbose=True)
 
 
